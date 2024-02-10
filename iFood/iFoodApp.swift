9918d3a4ee1411c840481cf3bcc0704a45ba4424
Roman Gudkov <rgudkoff@gmail.com>
2024-02-10 09:34:24 +0300

    Initial Commit

---
+++ iFood/iFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  iFoodApp.swift
+//  iFood
+//
+//  Created by Роман Гудков on 10.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct iFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
